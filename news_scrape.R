0623092706c9a3f383c96b30244753c82a392834
Zach Hrenko <zhrenko@cornerstone.com>
2025-09-16 13:15:55 -0600

    scraping code

---
+++ news_scrape.R
@@ -0,0 +1,11 @@
+library(httr2)
+
+req <- request("https://newsapi.org/v2/everything") |>
+  req_url_query(
+    q = '`"data science"`',
+    from = Sys.Date() - 1,
+    pageSize = 10,
+    apiKey = Sys.getenv("NEWS_API_KEY")
+  )
+
+req_perform(req, path = paste0("~/misc/newsy/data/", Sys.Date(), ".json"))
